3291f0e0b1b5abec2ebe2c456679c03b9f80bf0b
Ravi Jaydev Pilla <jaydev.ravi@gmail.com>
2019-03-21 17:07:42 -0400

    Added hard coded values for all the microservices

--- movie-catalog-service/src/main/java/com/java/moviecatalogservice/resouces/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/java/moviecatalogservice/resouces/MovieCatalogResource.java
@@ -1,15 +1,24 @@
 package com.java.moviecatalogservice.resouces;
 
+import java.util.Collections;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.moviecatalogservice.model.CatalogItem;
 
 @RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-	public List<CatalogItem> getCatalog(String userId){
-		
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+		return Collections.singletonList(
+				new CatalogItem("Avengers: Infinity War","This is a Marvel movie",10)
+				);
 	}
+
 }
